1f81ec5140e2b5b85f458390d154858b2faddd05
Bae Minseo <qoalstj01@gmail.com>
2024-06-30 23:44:47 +0900

    자바 기초 문법 문제 2
    자바 기초 문법 문제 2

---
+++ 01_java-assignment-master/src/main/java/com/ohgiraffers/assignment02/Application1.java
@@ -0,0 +1,20 @@
+package com.ohgiraffers.assignment02;
+
+public class Application1 {
+
+    public static void main(String[] args) {
+
+        /*
+         * 토끼가 당근 홀짝 게임에 참여했습니다.
+         * 당근 갯수를 선언하고,
+         * 선언한 당근 갯수가 짝수이면 "짝이군!",
+         * 짝수가 아니면 "홀이군!"을 출력하세요.
+         *
+         * ------- 출력 결과 화면 ----------
+         * 예) 정수 7를 선언한 경우
+         *
+         * 홀이군!
+         * */
+    }
+
+}
