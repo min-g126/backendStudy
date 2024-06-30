ccd11d632e5a1dca9d43d3518ac5163acb47744e
Bae Minseo <qoalstj01@gmail.com>
2024-07-01 00:53:19 +0900

    자바 기초 문법 문제 2-3
    자바 기초 문법 문제 2-3

---
+++ 01_java-assignment-master/src/main/java/com/ohgiraffers/assignment02/Application3.java
@@ -0,0 +1,19 @@
+package com.ohgiraffers.assignment02;
+
+public class Application3 {
+
+    public static void main(String[] args) {
+
+        /*
+         * 서울에서 패스트푸드의 늪에 빠진 토끼가 몸무게를 잽니다.
+         * 몸무게가 12kg 이상이면 비만이라고 출력되도록,
+         * 아니면 정상이라고 출력되로록 작성하세요. (삼항 연산자 사용)
+         *
+         * 예) 7 입력
+         *
+         * ---- 출력 결과 ---
+         * 정상!!!!!
+         * */
+    }
+
+}
