e96c1b33e01b4a531959dee8f1f9bcb2575502b0
min-g126 <qoalstj01@gmail.com>
2024-10-25 00:08:06 +0900

    test-initializr

---
+++ 10_Spring-Boot-master/test-initializr/test-initializr/src/main/java/com/ohgiraffers/testinitializr/TestInitializrApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.testinitializr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestInitializrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestInitializrApplication.class, args);
+	}
+
+}
